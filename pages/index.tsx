24dc6a4fe52fb15ad73c5acf2054e18c31aa3cf6
aegooby <aegooby@gmail.com>
2021-02-26 11:22:42 +0300

    Capital component name

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 import { useDeno } from "aleph";
 import * as React from "react";
 
-import Logo from "../components/logo.tsx";
+import Logo from "../components/Logo.tsx";
 import useCounter from "../lib/useCounter.ts";
 
 export default function Home() 
