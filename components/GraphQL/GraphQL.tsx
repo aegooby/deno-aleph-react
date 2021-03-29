c3adcd4d87e44cbf066347e37985a78c52e21a4f
aegooby <aegooby@gmail.com>
2021-03-29 21:30:12 +0300

    Added useClient hook

--- components/GraphQL/GraphQL.tsx
+++ components/GraphQL/GraphQL.tsx
@@ -8,10 +8,16 @@ export const Context = React.createContext(undefined as (Client | undefined));
 export const Provider = Context.Provider;
 export const Consumer = Context.Consumer;
 
-export async function useGraphQL(data?: string | Record<string, unknown>)
+export function useClient(): Client | undefined
 {
     const client = React.useContext(Context);
     if (!client) return undefined;
-    if (!data) return client;
+    return client;
+}
+
+export async function useGraphQL(data: string | Record<string, unknown>): Promise<Record<string, unknown> | undefined>
+{
+    const client = useClient();
+    if (!client) return undefined;
     return await client.fetch(data);
 }
