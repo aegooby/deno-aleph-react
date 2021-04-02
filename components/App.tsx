7c330e43445b3dbfd01a9746acea05c4b21971b3
aegooby <aegooby@gmail.com>
2021-04-02 13:30:05 +0300

    Removed Suspense from page loading

--- components/App.tsx
+++ components/App.tsx
@@ -2,7 +2,7 @@
 import * as React from "https://esm.sh/react";
 import * as ReactRouter from "https://esm.sh/react-router-dom";
 
-import { GraphQL, Suspense } from "./Core/Core.tsx";
+import { GraphQL } from "./Core/Core.tsx";
 
 import Index from "./Pages/Index.tsx";
 import NotFound from "./Pages/NotFound.tsx";
@@ -18,7 +18,7 @@ export default function App(props: Props)
         <GraphQL.Provider value={props.client}>
             <ReactRouter.Switch>
                 <ReactRouter.Route exact path="/">
-                    <Suspense fallback={<h1>Loading...</h1>}><Index /></Suspense>
+                    <Index />
                 </ReactRouter.Route>
                 <ReactRouter.Route component={NotFound} />
             </ReactRouter.Switch>
