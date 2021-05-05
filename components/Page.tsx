4eb7d3c10bcf93fed3c4ada6dc344aafdbaec4a2
aegooby <aegooby@gmail.com>
2021-05-05 22:58:56 +0300

    Added loading bar to Suspense

--- components/Page.tsx
+++ components/Page.tsx
@@ -1,52 +1,27 @@
 
 import * as React from "react";
 import * as ReactHelmet from "react-helmet";
-import nprogress from "nprogress";
 
-import { Suspense, throwOnClient } from "./Core/Core.tsx";
-
-interface FallbackProps
-{
-    element?: React.ReactElement | undefined;
-}
-
-function Fallback(props: FallbackProps)
-{
-    const effect = function () 
-    {
-        try { throwOnClient(); }
-        catch { nprogress.done(); }
-    };
-    React.useEffect(effect);
-    return props.element ?? <></>;
-}
+import { Suspense } from "./Core/Core.tsx";
 
 interface Props
 {
-    helmet: React.ReactElement;
+    helmet?: React.ReactElement;
     content: React.ReactElement;
     lazy?: true;
 }
 
 export default function Page(props: Props)
 {
-    try { throwOnClient(); } catch { React.useState(nprogress.start()); }
-    const suspenseContent =
-        <Suspense fallback={<Fallback />}>
-            {props.content}
-        </Suspense>;
-    const effect = function () 
-    {
-        try { throwOnClient(); }
-        catch { nprogress.done(); }
-    };
-    if (!props.lazy) React.useEffect(effect);
     const element: React.ReactElement =
         <>
             <ReactHelmet.Helmet>
                 {props.helmet}
+                <link rel="stylesheet" href="/nprogress.css" />
             </ReactHelmet.Helmet>
-            {props.lazy ? suspenseContent : props.content}
+            <Suspense fallback={<></>} loading>
+                {props.content}
+            </Suspense>
         </>;
     return element;
 }
